44217fa64b054257b0b12a35915fab8b119bc7ee
Kevin Yuan <yuanlin@qiyoukeji.com>
2016-05-19 15:23:35 +0800

    init commit

---
+++ TravisDemo/TravisDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravisDemo
+//
+//  Created by yuanlin on 16/5/19.
+//  Copyright © 2016年 com.aihan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravisDemo/TravisDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravisDemo
+//
+//  Created by yuanlin on 16/5/19.
+//  Copyright © 2016年 com.aihan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
