9ad6e5448eaac40eb9f979ff6043760a0599cf7a
Kevin Yuan <yuanlin@qiyoukeji.com>
2016-05-26 11:28:29 +0800

    add autobuild ipa

---
+++ scripts/sign-and-upload.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "This is a pull request. No deployment will be done."
+  exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+  echo "Testing on a branch other than master. No deployment will be done."
+  exit 0
+fi
+
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
+OUTPUTDIR="$PWD/build/Release-iphoneos"
+
+echo "***************************"
+echo "*        Signing          *"
+echo "***************************"
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+echo "***************************"
+echo "*        Publish          *"
+echo "***************************"
+fir p $OUTPUTDIR/$APP_NAME.ipa -T 0f494e1aa02928ad8fe7ce60c3bd96f0
